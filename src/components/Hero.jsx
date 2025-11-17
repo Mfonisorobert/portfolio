4969f8f46625eefe5a346999fd24daeab0a0389e
Mfonisorobert <mfonisorobert1@gmail.com>
2025-11-17 19:08:27 +0100

    portfolio website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -50,7 +50,7 @@ const Hero = () => {
           className='mx-auto mb-8 w-48 h-48 rounded-full object-cover'
           initial={{ opacity: 0, y: -80 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, ease: "easeOut" }}
+          transition={{ duration: 2, ease: "easeOut" }}
           whileHover={{ scale: 1.08 }}
         />
 
